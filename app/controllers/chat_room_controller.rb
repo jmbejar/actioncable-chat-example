4cf4818e9e96c45e02b0576b6e9a0ac1818e8b90
Jorge Bejar <jorge@wyeworks.com>
2015-07-09 12:13:33 -0300

    Implement mini chat room

--- app/controllers/chat_room_controller.rb
+++ app/controllers/chat_room_controller.rb
@@ -1,6 +1,9 @@
 class ChatRoomController < ApplicationController
   before_action :authenticate_user!
 
+  NUMBER_OF_MESSAGES = 15
+
   def index
+    @messages = Message.last(NUMBER_OF_MESSAGES)
   end
 end
