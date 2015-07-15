5022b0ab7e42b07dd93102a60ffea21f049ddc3b
Jorge Bejar <jorge@wyeworks.com>
2015-07-15 16:43:33 -0300

    Fix after sign up issue which was breaking the chat

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,6 +9,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
+        cookies.signed[:user_id] = resource.id
         redirect_to after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
