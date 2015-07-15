8b22f31f0e6b7905a9a2f71b5f14b00a4a94c8f0
Jorge Bejar <jorge@wyeworks.com>
2015-07-15 16:02:27 -0300

    Fix redirection after create new user

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,24 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        redirect_to after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        redirect_to after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+end
